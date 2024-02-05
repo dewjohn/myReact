7b96822556adc128a04b81f18ef1cb998319bbe2
john <deweizhong@outlook.com>
2024-02-05 21:23:41 +0800

    feat: 最小堆

--- src/scheduler/SchedulerMinHeap.js
+++ src/scheduler/SchedulerMinHeap.js
@@ -31,6 +31,54 @@ export function  siftUp(head, node, i) {
     }
 }
 
+export function pop(heap) {
+    if (heap.length === 0) {
+        return null
+    }
+    const first = heap[0]
+    const last = heap.pop()
+    if (last !== first) {
+        heap[0] = last
+        siftDown(heap, last, 0)
+    }
+}
+
+export function siftDown(heap, node, i) {
+    let index = i
+    const length = heap.length
+    const halfLength = length >>> 1
+    while (index < halfLength) {
+        const leftIndex = (index + 1) * 2 - 1
+        const left = heap[leftIndex]
+        const rightIndex = (index + 1) * 2 + 1
+        const right = heap[rightIndex]
+        // 如果左子树小于根节点
+        if (compare(left, node) < 0 ) {
+            // 如果此时右子树还比左子树还要小
+            if (rightIndex < length && compare(right, left) < 0) {
+                heap[index] = right
+                heap[rightIndex] = node
+                index = rightIndex
+            } else {
+                heap[index] = left
+                heap[leftIndex] = node
+                index = leftIndex
+            }
+        } else if (rightIndex < length && compare(right, node) < 0) {
+            heap[index] = right
+            heap[rightIndex] = node
+            index = rightIndex
+        } else {
+            // 子树都比根节点大
+            return
+        }
+    }
+}
+
+export function peek(heap) {
+    return heap.length === 0 ? null : heap[0]
+}
+
 export function compare(a, b) {
     const diff = a.sortIndex - b.sortIndex
     return diff !== 0 ? diff : a.id - b.id
